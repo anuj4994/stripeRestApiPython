66b1da278d2c04e1fe4675271b410181de616bd4
anuj4994 <anuj.shah.94@gmail.com>
2017-08-06 16:04:10 -0700

    Adding more methods

--- stripeRest.py
+++ stripeRest.py
@@ -16,8 +16,28 @@ class makePayment(Resource):
         print "Attempting charge..."
         resp = stripe.Charge.create(amount=amount, currency='usd', source=source, description=description)
         print 'Success'
+class saveCustomer(Resource):
+        def post(self):
+                email = request.form["email"]
+                source = request.form["source"]
+        		stripe.api_key = '<STRIPE SECRET KEY>'#starting with sk...
+    	        customer = stripe.Customer.create(email=email,source=source)
+                print 'Success'
+                return customer.id
+
+class payWithCustomer(Resource):
+        def post(self):
+                amount = request.form["amount"]
+                customerId = request.form["customerId"]
+                description = request.form['description']
+		        stripe.api_key = '<STRIPE SECRET KEY>'#starting with sk...
+                charge = stripe.Charge.create( amount=amount,currency="usd",customer=customerId,description=description)
+
+
 
 api.add_resource(makePayment, '/makePayment/')
+api.add_resource(saveCustomer, '/saveCustomer/')
+api.add_resource(payWithCustomer, '/payWithCustomer/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='7002')
